f3fb5f44cfe13d978b832f4d5abaa0de868e4131
om151204 <ommishra97277@gmail.com>
2026-02-17 17:48:15 +0530

    Update: Added new endpoints for transactions

--- finance/api/transactions.py
+++ finance/api/transactions.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, status, HTTPException, Query
-from finance.api.base_models import Transaction
+from finance.api.base_models import Transaction,TransactionUpdate,Category
 from finance.database import db
 router = APIRouter()
 
@@ -15,6 +15,12 @@ def serialize(transaction):
         "tags": transaction["tags"],
     }
 
+def serialize_category(category):
+    return {
+        "id": str(category["_id"]),
+        "name": category["name"]
+    }
+
 @router.post("/",status_code=status.HTTP_201_CREATED)
 def create_transaction(transaction:Transaction):
     try:
@@ -23,6 +29,15 @@ def create_transaction(transaction:Transaction):
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
+@router.get("/search",status_code=status.HTTP_200_OK)
+def search_transactions(query:str):
+    if not query:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Search query is required")
+    results = db.search_transactions(query)
+    transactions = [serialize(t) for t in results]
+    if not transactions:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No matching transaction not found")
+    return transactions
 @router.get("/",status_code=status.HTTP_200_OK)
 def get_transactions(page_size:int=Query(10,le=100)):
     transactions = db.get_all_transactions(page_size)
@@ -45,6 +60,80 @@ def delete_transaction(id:str):
         return res
     except Exception:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid transaction ID")
+
+@router.delete("/bulk/{category}",status_code=status.HTTP_202_ACCEPTED)
+def bulk_delete_transaction(category:str):
+    result = db.delete_transaction_many(category)
+    if result:
+        return{
+            "message":"Transaction deleted successfully",
+            "deleted_count": result.deleted_count,
+        }
+    else:
+        return {"Message":"Transaction could not be deleted"}
+
+@router.patch("/{id}",status_code=status.HTTP_200_OK)
+def update_transaction(id:str,payload:TransactionUpdate):
+    try:
+        updated_transaction = db.update_transaction(id,payload.model_dump(exclude_unset=True))
+        if not updated_transaction:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Transaction not found")
+        return serialize(updated_transaction)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid transaction ID")
+
+
+@router.post("/get_transactions/", status_code=status.HTTP_201_CREATED,tags=["categories"])
+def create_category(category: Category):
+    existing = db.cat_collection.find_one({"name": category.name})
+    if existing:
+        raise HTTPException(
+            status_code=400,
+            detail="Category already exists"
+        )
+
+    result = db.create_category(category.model_dump())
+    created = db.cat_collection.find_one({"_id": result.inserted_id})
+
+    return serialize_category(created)
+
+
+@router.get("/transactions/",tags=["categories"])
+def get_categories():
+    categories = db.get_categories()
+    return [serialize(cat) for cat in categories]
+
+
+# PATCH
+@router.patch("/transactions/{name}",tags=["categories"])
+def update_category(name: str, payload: Category):
+
+    update_data = payload.model_dump()
+
+    updated = db.update_category(name, update_data)
+
+    if not updated:
+        raise HTTPException(
+            status_code=404,
+            detail="Category not found"
+        )
+
+    return serialize(updated)
+
+
+# DELETE
+@router.delete("/transactions/{name}",tags=["categories"])
+def delete_category(name: str):
+
+    result = db.delete_category(name)
+
+    if result.deleted_count == 0:
+        raise HTTPException(
+            status_code=404,
+            detail="Category not found"
+        )
+
+    return {"message": "Category deleted successfully"}
 
 
 
